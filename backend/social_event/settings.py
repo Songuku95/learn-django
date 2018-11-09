2917ebb916d9ae6a2bdf19b02a5e270c21e47189
Nguyễn Duy Khánh <duykhanh.nguyen@ved.com.vn>
2018-11-09 10:23:40 +0700

    Config to use MySQL DB

--- backend/social_event/settings.py
+++ backend/social_event/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'social_event.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'NAME': 'event_social_db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
