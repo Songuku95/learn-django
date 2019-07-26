a1fa78f414c28cfff98e366695fdaf54c59325e1
Nguyễn Duy Khánh <duykhanh.nguyen@ved.com.vn>
2019-07-26 16:35:10 +0700

    Test

--- liquidflare.js
+++ liquidflare.js
@@ -1,8 +1,8 @@
-(function() {
+var liquidflare = function() {
 	var x = document.createElement("a");
 	x.setAttribute("name", "liquidflare");
 	x.setAttribute("href", "/check_bot");
 	document.body.appendChild(x);
-	alert("Hello! I am an alert box!!");
-})();
+};
 
+document.addEventListener('DOMContentLoaded', liquidflare, false);
